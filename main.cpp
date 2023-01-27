a354422b6a0678f89b9188325349c0a115b20238
1211101376 <85892411+Idksouls@users.noreply.github.com>
2023-01-27 23:56:10 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// ********************************************************* 
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
+// Year: Trimester 1, 2022/23 (T2215) 
+// Lab: T11L 
+// Names: ISAIAH WONG TERJIE | JAVIER AUSTIN ANAK JAWA 
+// IDs: 1211101376 | MEMBER_ID_2 | MEMBER_ID_3 
+// Emails: 1211101376@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
+// Phones: 011-2327-1713 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// ********************************************************* 
+
+// TODO: Fill in the missing information above and delete this line.
+
+#include "pf/helper.cpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib> // for system()
+#include <ctime> // for time() in srand( time(NULL) );
+#include <iomanip> // for setw()
+using namespace std;
+
+class board
+
+int main()
+{
+    cout << "Assignment (Part 1)" << endl;
+    cout << "Let's Get Started!" << endl;
+    pf::Pause();
+}
