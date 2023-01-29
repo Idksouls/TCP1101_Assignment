19161d77c4bcf8dae38ccac002501ed4b0142444
1211101376 <85892411+Idksouls@users.noreply.github.com>
2023-01-29 15:49:28 +0800

    Update main.cpp
    added Aliens in the middle and going to update alien movement in a bit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-// ********************************************************* 
-// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
-// Year: Trimester 1, 2022/23 (T2215) 
-// Lab: T11L 
-// Names: ISAIAH WONG TERJIE | JAVIER AUSTIN ANAK JAWA 
-// IDs: 1211101376 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: 1211101376@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: 011-2327-1713 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
-// ********************************************************* 
+// *********************************************************
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS
+// Year: Trimester 1, 2022/23 (T2215)
+// Lab: T11L
+// Names: ISAIAH WONG TERJIE | JAVIER AUSTIN ANAK JAWA
+// IDs: 1211101376 | 1211100857
+// Emails: 1211101376@student.mmu.edu.my | 1211100857@student.mmu.edu.my
+// Phones: 011-2327-1713 | MEMBER_PHONE_2 | MEMBER_PHONE_3
+// *********************************************************
 
 // TODO: Fill in the missing information above and delete this line.
 
@@ -15,15 +15,130 @@
 #include <string>
 #include <vector>
 #include <cstdlib> // for system()
-#include <ctime> // for time() in srand( time(NULL) );
+#include <ctime>   // for time() in srand( time(NULL) );
 #include <iomanip> // for setw()
 using namespace std;
 
-class board
+class Board
+{
+private:
+    vector<vector<char>> map_;
+    int Row_, Col_;
+    int alienRow, alienCol;
+
+
+public:
+    Board();
+    void init(int Row, int Col);
+    void display() const;
+};
+
+Board::Board()
+{
+    Row_ = 15;
+    Col_ = 5;
+    init (Row_,Col_);
+    alienRow = Col_ / 2;
+    alienCol = Row_ / 2;
+}
+
+void Board::init(int Row, int Col)
+{
+    Row_ = Row;
+    Col_ = Col;
+    char objects[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    int noOfObjects = 10; // number of objects in the objects array
+    // create dynamic 2D array using vector
+    map_.resize(Col_); // create empty rows
+    for (int i = 0; i < Col_; ++i)
+
+    {
+        map_[i].resize(Row_); // resize each row
+    }
+    // put random characters into the vector array
+    for (int i = 0; i < Col_; ++i)
+    {
+        for (int j = 0; j < Row_; ++j)
+        {
+            int objNo = rand() % noOfObjects;
+            map_[i][j] = objects[objNo];
+        }
+    }
+    map_[Col_/2][Row_/2] = 'A';
+}
+
+
+void Board::display() const
+{
+    cout << " --__--__--__--__--__--__--__--_--__--__--__--__--__--__--_" << endl;
+    cout << " = Hello Adventurer are you ready to kill some ZOMBIESS!!! =" << endl;
+    cout << " __--__--__--__--__--__--__--__--__--__--__--__--__--__--__" << endl;
+
+    // for each row
+    for (int i = 0; i < Col_; ++i)
+    {
+        // display upper border of the row
+        cout << " ";
+        for (int j = 0; j < Row_; ++j)
+        {
+            cout << "+-";
+        }
+        cout << "+" << endl;
+        // display row number
+        cout << setw(2) << (Col_ - i);
+        // display cell content and border of each column
+        for (int j = 0; j < Row_; ++j)
+        {
+            cout << "|" << map_[i][j];
+        }
+        cout << "|" << endl;
+    }
+    // display lower border of the last row
+    cout << " ";
+    for (int j = 0; j < Row_; ++j)
+    {
+        cout << "+-";
+    }
+    cout << "+" << endl;
+    // display column number
+    cout << " ";
+    for (int j = 0; j < Row_; ++j)
+    {
+        int digit = (j + 1) / 10;
+        cout << " ";
+        if (digit == 0)
+            cout << " ";
+        else
+            cout << digit;
+    }
+    cout << endl;
+    cout << " ";
+    for (int j = 0; j < Row_; ++j)
+    {
+        cout << " " << (j + 1) % 10;
+    }
+    cout << endl
+         << endl;
+
+}
+
 
 int main()
 {
+    int Row,Col;
     cout << "Assignment (Part 1)" << endl;
     cout << "Let's Get Started!" << endl;
+    srand(time(NULL));
+    cout << "Please Enter Your Desired Dimensions Of The Board"<<endl;
+    cout << "Number of Rows: "<<endl;
+    cin >> Row;
+    cout << "Number of Columns: "<<endl;
+    cin >> Col;
+    
+    Board board;
+    board.init(Row,Col);
+    board.display();
+
+
     pf::Pause();
 }
